58510fc83114ddb26272c0fa82fc3c0317283b61
KristofferNyvoll <kristnyv@stud.ntnu.no>
2022-12-19 18:58:55 +0100

    general layout of pages

---
+++ components/UpcomingComp.tsx
@@ -0,0 +1,31 @@
+import styles from "../styles/pages.module.css";
+import Button from "./Button";
+
+interface CompetitionProps {
+  title: string;
+  date: string;
+  time: string;
+  location: string;
+  registrationButton?: JSX.Element;
+  buttonText?: string;
+}
+
+export default function UpcomingCompetition({
+  title,
+  date,
+  time,
+  location,
+  registrationButton,
+  buttonText,
+}: CompetitionProps) {
+  return (
+    <div className={styles.nextCompetition}>
+      <h3>Hva: {title}</h3>
+      <h3>Dato: {date}</h3>
+      <h3>Tidspunkt: {time}</h3>
+      <h3>Hvor: {location}</h3>
+
+      {registrationButton && buttonText && <Button text={buttonText} />}
+    </div>
+  );
+}
--- pages/idi-open.tsx
+++ pages/idi-open.tsx
@@ -3,18 +3,19 @@ import styles from "../styles/pages.module.css";
 import RegistrationForm from "../components/RegistrationForm";
 import Button from "../components/Button";
 import Image from "next/image";
+import Title from "../components/Title";
 
 export default function IDIOpen() {
   const [registrationOpen, setRegistrationOpen] = useState(true);
 
   return (
     <section className={styles.section}>
-      <h1 className={styles.title}>IDI Open</h1>
+      <Title text={"~/idi-open"} />
       <div className={styles.nextCompetition}>
-        <h1>IDI Open 2023</h1>
-        <h2>24.April</h2>
-        <h2>11:00-16:00</h2>
-        <h3>NTNU Gløshaugen</h3>
+        <h3>Hva: IDI Open 2023</h3>
+        <h3>Dato: 24.April</h3>
+        <h3>Tidspunkt: 11:00-16:00</h3>
+        <h3>Hvor: Realfagbygget, NTNU Gløshaugen</h3>
 
         {registrationOpen && <Button text="Registrer lag" />}
       </div>
@@ -22,15 +23,23 @@ export default function IDIOpen() {
       <div className={styles.description}>
         <div className={styles.logo}>
           <Image
-            src="/vercel.svg"
+            src="/idi_open_logo.jpg"
             alt="IDI Open Logo"
             className={styles.idiopenLogo}
-            width={100}
-            height={24}
+            width={300}
+            height={300}
           />
         </div>
         <div className={styles.descriptionText}>
           <h2>Hva er IDI Open?</h2>
+          <p>
+            Det som begynte som en treningsarena for NCPC (som holdes på høsten)
+            ble til en årlig programmeringskonkurranse som har blitt gjennomført
+            siden 2012. Konkurransen er åpen for alle som vil delta, både
+            tilstede på NTNU og remote (kun de på NTNU får premier). Oppgavene
+            lages av ildsjeler som virkelig brenner for
+            konkurranseprogrammering.
+          </p>
         </div>
       </div>
       <div className={styles.registration}>
